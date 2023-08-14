d2c977de0058bd7fce0bbed6b9424c893b6b9aa0
Manir Uddin <manir22sust@gmail.com>
2023-08-14 13:07:34 +0200

    feat:hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,29 @@
 import * as React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/Layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 export default function Home() {
   return (
     <Layout>
-      <h1>hello</h1>
-      <Link to="/about"> about</Link>
-      <Link to="/company/history"> history</Link>
+      <main className="page">
+        <header className="hero">
+          <StaticImage
+            src="../assets/images/main.jpeg"
+            alt="eggs"
+            className="hero-img"
+            placeholder="tracedSVG"
+            layout="fullWidth"
+          ></StaticImage>
+
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>simply recipes</h1>
+              <h4>no fluff, just recipes</h4>
+            </div>
+          </div>
+        </header>
+      </main>
     </Layout>
   );
 }
