0fafb821a118e3c1907a093d468efafcf013537c
Manir Uddin <manir22sust@gmail.com>
2023-08-24 13:53:22 +0200

    TagsList components and Tags Page

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,7 +1,39 @@
 import React from "react";
 import Layout from "../components/Layout";
-const Tags = () => {
-  return <Layout>tags</Layout>;
+import { graphql, Link } from "gatsby";
+import setupTags from "../utils/setupTags";
+
+const Tags = ({ data }) => {
+  const newTags = setupTags(data.allContentfulRecipe.nodes);
+  return (
+    <Layout>
+      <main className="page">
+        <section className="tags-page">
+          {newTags.map((tag, index) => {
+            const [text, value] = tag;
+            return (
+              <Link to={`/${text}`} key={index} className="tag">
+                <h5>{text} </h5>
+                <p>{value} </p>
+              </Link>
+            );
+          })}
+        </section>
+      </main>
+    </Layout>
+  );
 };
+
+export const query = graphql`
+  query {
+    allContentfulRecipe {
+      nodes {
+        content {
+          tags
+        }
+      }
+    }
+  }
+`;
 
 export default Tags;
